f6c97d5e154990ee76e64086a73138bf0aa9eedb
Kashif56 <8bpcoins4u@gmail.com>
2025-06-27 18:49:12 +0500

    fix: add CSRF token to contact form and correct contact URL path

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.landing, name='landing'),
-    path('/contact-us/', views.contact_us, name='contact'),
+    path('contact-us/', views.contact_us, name='contact'),
     path('payment/<int:plan_id>/', views.payment, name='payment'),
 ]
